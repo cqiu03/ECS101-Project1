99ccdf42dd81388cb97fc810a250691d810e4778
Cheng Qiu <cqiu03@syr.edu>
2021-10-12 14:17:18 -0400

    added new line

--- encrypt.py
+++ encrypt.py
@@ -1,23 +1,12 @@
 class Encrypt:
-    #We need a function to somehow preprocess the text to include the things that we want
-    #for example if "and" is a reoccuring world and its value is 1 we need to change all and into 1
-    def encrypt(self):
-        encrypted = ""
-        for char in self.text:
-            encrypted += self.CHAR_CONVERSION[char]
-        encrypted = (str)(len(encrypted)) + "." + encrypted
-        return encrypted
-
-    def export(self):
-        with open('binary_result.txt', 'w', newline='') as f:
-            f.write(self.encrypt())
-        f.close()
-
     def __init__(self, filename):
         self.file = open(filename)
         self.text = self.file.read()
         #Have a special dictionary that contains the special cases so make it easier
         #to iterate through it
+
+        #self.CHAR_SP = ['and', 'the']
+
         self.CHAR_CONVERSION = {"a":"00001", "e":"00010", "i":"00011", "o":"00100", "u":"00101",
         "t":"00110", "s":"00111", "W":"01000", "T":"01001", "I":"01010",
         "A":"01011", "H":"01100", "S":"01101", "B":"01110", "n":"01111",
@@ -31,6 +20,30 @@ class Encrypt:
         "q":"1011100", "r":"1011101", "v":"1011110", "x":"1011111",
         "y":"1100000", "z":"1100001", "w":"1100010","j":"1101010",
         ".":"1100011", " ":"00000", "-":"1100101", 'b': '1101001',
-        '"':'1101011', ',': '1100100', "'": "1100111"}
+        '"':'1101011', ',': '1100100', "'": "1100111","\\n":"1101000"}
 
         self.export()
+
+    #We need a function to somehow preprocess the text to include the things that we want
+    #for example if "and" is a reoccuring world and its value is 1 we need to change all and into 1
+
+    #use recursion to check for the length of the key and check custom word first
+    def encrypt(self):
+        encrypted = ""
+        x = 0
+        while x < len(self.text):
+        #for x in range (len(self.text)):
+            char = self.text[x]
+            if self.text[x:x+2] == "\\n":
+                #print(self.text[x:x+2])
+                char = "\\n"
+                x+=1
+            encrypted += self.CHAR_CONVERSION[char]
+            x+=1
+        encrypted = (str)(len(encrypted)) + "." + encrypted
+        return encrypted
+
+    def export(self):
+        with open('binary_result.txt', 'w', newline='') as f:
+            f.write(self.encrypt())
+        f.close()
