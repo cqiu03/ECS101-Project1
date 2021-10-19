e1379ffb2fc0100e0a438598ea9254b627d4aaf6
cqiu03 <cqiu03@syr.edu>
2021-10-19 09:52:16 -0400

    change a little bit

--- driver_file.py
+++ driver_file.py
@@ -4,6 +4,6 @@ from decrypt import Decrypt
 #Read in a txt file containing str and convert it to binary
 Encrypt("Input_Text.txt")
 
-#Read in a txt file containing binary and convert it to str
+#Read in a txt file containing the binary and convert it to str
 Decrypt("binary_result.txt")
 print("----------DONE------------")
